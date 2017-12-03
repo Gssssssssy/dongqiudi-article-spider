618b306942c8a6df6010a6ed2eaa34ef5ab2adbc
Gssssssssy <coder.gsy@gmail.com>
2017-12-03 10:21:21 +0800

    批量插入

--- dongqiudi/dongqiudi/pipelines.py
+++ dongqiudi/dongqiudi/pipelines.py
@@ -18,14 +18,18 @@ sys.setdefaultencoding('utf-8')
 class DongQiuDiPipeline(object):
     def __init__(self):
         self.session = DBSession()
-        self.article_id_seen = set()
+        self.article_id_seen = set(self.session.query(Articles.article_id).all())
+        self.results = []
 
     def process_item(self, item, spider):
+        nt = datetime.datetime.now()
         if item['article_id'] in self.article_id_seen:  # 除重
             raise DropItem("Duplicate item found: %s" % item)
+        else:
+            art = Articles(created_time=nt, last_updated=nt, **item)
+            self.results.append(art)
         self.article_id_seen.add(item['article_id'])
 
-        nt = datetime.datetime.now()
         if item['published_at']:  # 投毒处理
             published_at_2_datetime = datetime.datetime.strptime(item['published_at'], "%Y-%m-%d %H:%M:%S")
             if published_at_2_datetime.year > nt.year:
@@ -37,17 +41,24 @@ class DongQiuDiPipeline(object):
             item['source'] = u''
 
         # 入库
-        queryset = self.session.query(Articles).filter(Articles.article_id == item['article_id']).first()
-        if not queryset:
-            self.session.add(Articles(created_time=nt, last_updated=nt, **item))
-        else:
-            queryset.comments = item['comments']
-            queryset.share = item['share']
-            queryset.last_updated = nt
-            if item.get('visit_total'):
-                queryset.visit_total = item['visit_total']
+        # queryset = self.session.query(Articles).filter(Articles.article_id == item['article_id']).first()
+        # if not queryset:
+        #     self.session.add(Articles(created_time=nt, last_updated=nt, **item))
+        # else:
+        #     queryset.comments = item['comments']
+        #     queryset.share = item['share']
+        #     queryset.last_updated = nt
+        #     if item.get('visit_total'):
+        #         queryset.visit_total = item['visit_total']
+        # self.session.commit()
+        # return item
+        if len(self.results) >= 100:
+            self.session.bulk_save_objects(self.results)
         self.session.commit()
-        return item
 
     def close_spider(self, spider):  # 上下文管理
+        if not self.results:
+            self.session.bulk_save_objects(self.results)
+        self.session.commit()
+
         self.session.close()
