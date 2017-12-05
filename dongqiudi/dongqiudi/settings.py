c74342a19c882e08bf388f23edaa7231f78ca7cf
Gssssssssy <coder.gsy@gmail.com>
2017-12-05 21:56:20 +0800

    关闭代理

--- dongqiudi/dongqiudi/settings.py
+++ dongqiudi/dongqiudi/settings.py
@@ -26,7 +26,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 1.5
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -51,11 +51,11 @@ DOWNLOAD_DELAY = 0.5
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-# 'dongqiudi.middlewares.MyCustomDownloaderMiddleware': 543,
-#  'dongqiudi.middlewares.RandomUserAgentMiddleware': 400,
- 'dongqiudi.middlewares.RandomProxyMiddlerware': 100,
-}
+# DOWNLOADER_MIDDLEWARES = {
+    # 'dongqiudi.middlewares.MyCustomDownloaderMiddleware': 543,
+    #  'dongqiudi.middlewares.RandomUserAgentMiddleware': 400,
+    # 'dongqiudi.middlewares.RandomProxyMiddlerware': 100,
+# }
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -106,4 +106,4 @@ MYSQL_DB = {
 }
 LOG_LEVEL = "INFO"
 
-PROXIES = [('60.190.96.190', '808'), ('113.87.161.176', '8088'), ('222.174.168.102', '53281')]
+# PROXIES = [('60.190.96.190', '808'), ('113.87.161.176', '8088'), ('222.174.168.102', '53281')]
